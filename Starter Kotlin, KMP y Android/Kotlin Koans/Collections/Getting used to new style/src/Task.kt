5c5c45d8f4d26683c9b12ebcffee749755b94ec2
DeTiveNC <116792124+DeTiveNC@users.noreply.github.com>
2024-12-04 20:18:21 +0100

    Add kotlin Learner from jetbrains IDE

--- Starter Kotlin, KMP y Android/Kotlin Koans/Collections/Getting used to new style/src/Task.kt
+++
@@ -1,8 +0,0 @@
-fun doSomethingWithCollection(collection: Collection<String>): Collection<String>? {
-
-    val groupsByLength = collection.groupBy { s -> s.length }
-
-    val maximumSizeOfGroup = groupsByLength.values.maxOfOrNull { group -> group.size }
-
-    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
-}
